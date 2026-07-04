d7629bfc5e75a7b1cb2f493e73b97e5f44f4771e
heatoz <89743736+heatoz@users.noreply.github.com>
2026-07-04 18:11:33 -0300

    feat: add icon to the main window

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
-import sys
-
-from PySide6.QtWidgets import QApplication
-
 from bbcodegen.ui.mainwindow import MainWindow
-
+from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
+from pathlib import Path
+import sys
 
 def main():
     app = QApplication(sys.argv)
+
+    icon_path = Path(__file__).parent.parent / "assets" / "favicon.ico"
+    app.setWindowIcon(QIcon(str(icon_path)))
+
     window = MainWindow()
     window.show()
     _ = app.exec()
-    
-
 
 if __name__ == "__main__":
     main()
